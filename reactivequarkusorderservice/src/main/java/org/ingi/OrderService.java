dcf92d828219c8b86c7e8f8e829db0247aff1020
ingmarfjolla <ingmar.fjolla09@myhunter.cuny.edu>
2023-02-23 18:32:25 -0500

    add debezium back

--- reactivequarkusorderservice/src/main/java/org/ingi/OrderService.java
+++ reactivequarkusorderservice/src/main/java/org/ingi/OrderService.java
@@ -1,9 +1,8 @@
 package org.ingi;
 import com.fasterxml.jackson.databind.JsonNode;
-//import io.debezium.outbox.reactive.quarkus.internal.DebeziumOutboxHandler;
+import io.debezium.outbox.reactive.quarkus.internal.DebeziumOutboxHandler;
 
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
-import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional;
 import io.quarkus.logging.Log;
 import io.quarkus.vault.runtime.client.Shared;
 import io.quarkus.vault.runtime.client.VaultClient;
@@ -14,7 +13,7 @@ import jakarta.inject.Inject;
 import org.hibernate.reactive.mutiny.Mutiny;
 import org.ingi.repository.OrderRepository;
 import org.ingi.repository.models.Order;
-//import org.ingi.cdc.OrderCreatedEvent;
+import org.ingi.cdc.OrderCreatedEvent;
 
 
 import java.util.Objects;
@@ -24,8 +23,8 @@ import java.util.UUID;
 
 @ApplicationScoped
 public class OrderService {
-//    @Inject
-//    DebeziumOutboxHandler handler;
+    @Inject
+    DebeziumOutboxHandler handler;
     @Inject @Shared
     VaultClient client;
 //    @Inject
@@ -39,9 +38,9 @@ public class OrderService {
     @WithTransaction
     public Uni<Order> create(Order order){
         Log.info(Thread.currentThread().getName());
-        return this.orderRepository.persistAndFlush(order);
+        return this.orderRepository.persistAndFlush(order)
 //                .invoke(()->this.beginExample(order));
-//                .call(() -> handler.persistToOutbox(OrderCreatedEvent.of(order)));
+                .call(() -> handler.persistToOutbox(OrderCreatedEvent.of(order)));
     }
 
 
--- reactivequarkusorderservice/src/main/java/org/ingi/cdc/OrderCreatedEvent.java
+++ reactivequarkusorderservice/src/main/java/org/ingi/cdc/OrderCreatedEvent.java
@@ -1,73 +1,73 @@
-//package org.ingi.cdc;
-//import com.fasterxml.jackson.databind.JsonNode;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.node.ArrayNode;
-//import com.fasterxml.jackson.databind.node.ObjectNode;
-//import io.debezium.outbox.quarkus.ExportedEvent;
-//import org.ingi.repository.models.Order;
-//import org.ingi.repository.models.OrderItem;
-//import org.jboss.logging.Logger;
-//
-//import java.time.Instant;
-//
-//public class OrderCreatedEvent implements ExportedEvent{
-//
-//    private static ObjectMapper mapper = new ObjectMapper();
-//
-//    private final String id;
-//    private final JsonNode order;
-//    private final Instant timestamp;
-//
-//    private OrderCreatedEvent(String id, JsonNode order) {
-//        this.id = id;
-//        this.order = order;
-//        this.timestamp = Instant.now();
-//    }
-//
-//    @Override
-//    public String getAggregateId() {
-//        return String.valueOf(id);
-//    }
-//
-//    @Override
-//    public String getAggregateType() {
-//        return "Order";
-//    }
-//
-//    @Override
-//    public JsonNode getPayload() {
-//        return order;
-//    }
-//
-//    @Override
-//    public String getType() {
-//        return "OrderCreated";
-//    }
-//
-//    public Instant getTimestamp() {
-//        return timestamp;
-//    }
-//
-//    public static OrderCreatedEvent of(Order order) {
-//        Logger.getLogger(OrderCreatedEvent.class).infof("Creating OrderCreatedEvent for Order: %s", order);
-//
-//        ObjectNode asJson = mapper.createObjectNode()
-//                .put("orderNumber", order.getOrderNumber())
-//                .put("customer", order.getCustomer())
-//                .put("customerId", order.getCustomerId());
-//
-//        ArrayNode items = asJson.putArray("items");
-//
-//        for (OrderItem orderLine : order.getOrderItems()) {
-//            ObjectNode lineAsJon = mapper.createObjectNode()
-////                    .put("id", orderLine.getId().toString())
-//                    .put("skuId", orderLine.getSkuId())
-//                    .put("type", orderLine.getType())
-//                    .put("name", orderLine.getName())
-//                    .put("quantity", orderLine.getQuantity());
-//            items.add(lineAsJon);
-//        }
-//
-//        return new OrderCreatedEvent(order.getOrderNumber(), asJson);
-//    }
-//}
+package org.ingi.cdc;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.debezium.outbox.quarkus.ExportedEvent;
+import org.ingi.repository.models.Order;
+import org.ingi.repository.models.OrderItem;
+import org.jboss.logging.Logger;
+
+import java.time.Instant;
+
+public class OrderCreatedEvent implements ExportedEvent{
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    private final String id;
+    private final JsonNode order;
+    private final Instant timestamp;
+
+    private OrderCreatedEvent(String id, JsonNode order) {
+        this.id = id;
+        this.order = order;
+        this.timestamp = Instant.now();
+    }
+
+    @Override
+    public String getAggregateId() {
+        return String.valueOf(id);
+    }
+
+    @Override
+    public String getAggregateType() {
+        return "Order";
+    }
+
+    @Override
+    public JsonNode getPayload() {
+        return order;
+    }
+
+    @Override
+    public String getType() {
+        return "OrderCreated";
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public static OrderCreatedEvent of(Order order) {
+        Logger.getLogger(OrderCreatedEvent.class).infof("Creating OrderCreatedEvent for Order: %s", order);
+
+        ObjectNode asJson = mapper.createObjectNode()
+                .put("orderNumber", order.getOrderNumber())
+                .put("customer", order.getCustomer())
+                .put("customerId", order.getCustomerId());
+
+        ArrayNode items = asJson.putArray("items");
+
+        for (OrderItem orderLine : order.getOrderItems()) {
+            ObjectNode lineAsJon = mapper.createObjectNode()
+//                    .put("id", orderLine.getId().toString())
+                    .put("skuId", orderLine.getSkuId())
+                    .put("type", orderLine.getType())
+                    .put("name", orderLine.getName())
+                    .put("quantity", orderLine.getQuantity());
+            items.add(lineAsJon);
+        }
+
+        return new OrderCreatedEvent(order.getOrderNumber(), asJson);
+    }
+}
